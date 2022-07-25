6b7260d3a9679e8fd0387db9d1d0d3b48e2574e5
morgussian <simpsonwolfgang@mail.com>
2022-07-25 21:03:04 +0200

    le panier envoie un array et un contact, l'API renvoie une réponse

--- front/js/form.js
+++ front/js/form.js
@@ -1,13 +1,17 @@
-//récupérer l'élément formulaire
-let form = document.querySelector('form');
-form.method = 'post';
 
 // récupérer les inputs d'après leur attribut name
-let firstName = form.firstName;
-let lastName = form.lastName;
-let address = form.address;
-let city = form.city;
-let email = form.email;
+class User {
+    constructor(firstName, lastName, email, address, city) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.address = address
+        this.city = city
+    }
+}
+
+var user = new User();
+var form = document.querySelector('form');
 
 //l'ensemble des messages <p>
 let fields = document.querySelectorAll('.cart__order__form__question p');
@@ -27,27 +31,33 @@ let addressReg = new RegExp(/^[a-zA-Z0-9\s,'-]*$/);
 //regexp pour le mail
 let mailReg = new RegExp (/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i);
 
-//objet user
-let user = {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    address: address.value,
-    city: city.value,
-    email: email.value
-}
+form.addEventListener('change', function() {
+    this.user = new User(
+        form.firstName.value,
+        form.lastName.value,
+        form.email.value,
+        form.address.value,
+        form.city.value,
+    )
+})
 
 //methode post envoyer user et panier
-orderBtn.addEventListener('submit', async function(e){
+form.addEventListener('submit', async function(e){
     
-    
+    e.preventDefault()
+
+    user = this.user
     let cart = getCart();
-    let response = await fetch('http://localhost:3000/api/order', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user), 
-    body: JSON.stringify(cart)
+    let response = await fetch('http://localhost:3000/api/products/order', 
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            contact: user,
+            products: getCart().map(function(product) { return product.id})
+        })
     });
 
     //ajouter un lien vers la page confirmation ça marche pas.
@@ -71,28 +81,31 @@ function check(input, regEx){
         let msg = input.nextElementSibling//document.getElementById(msgId+ 'ErrorMsg');
         
         //boucle pour afficher les messages
-        if(regEx.test(input.value)){
+        if(regEx.test(input.value)) {
             msg.innerText = successMsg;
             msg.style.color = 'limeGreen';
             return true;
-        } else if (regEx == mailReg){
-                msg.innerText = mailError;
-                return false;
-        } else if (regEx == nameCityReg){
-                    msg.innerText = wordError;
-                    return false;
+        } else if (regEx == mailReg) {
+            msg.innerText = mailError;
+            return false;
+        } else if (regEx == nameCityReg) {
+            msg.innerText = wordError;
+            return false;
         } else {
-                    msg.innerText = addressError;
-                    return false;
+            msg.innerText = addressError;
+            return false;
         }
     });
 }
 
-check(firstName, nameCityReg);
-check(lastName, nameCityReg);
-check(address, addressReg);
-check(city, nameCityReg);
-check(email, mailReg);
+function addRegexListenerToFormInputs() {
+    check(form.firstName, nameCityReg);
+    check(form.lastName, nameCityReg);
+    check(form.address, addressReg);
+    check(form.city, nameCityReg);
+    check(form.email, mailReg);
+}
+addRegexListenerToFormInputs()
 
 function validAllFields() {
     if(check(firstName, nameCityReg) && check(lastName, nameCityReg) && check(address, addressReg) && check(city, nameCityReg) && check(email, mailReg) == true){
